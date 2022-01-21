3d8a0f86449db407aedc49af2016a2ac932de227
Stefano Ruth <stefanoruth@users.noreply.github.com>
2022-01-21 08:53:47 +0100

    wip

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Loading stere dotfiles"
+echo -e "Loading steru dotfiles\n"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -13,10 +13,23 @@ function linkFile() {
 function addToFile() {
     FILE=$1
 
-    cat $SCRIPT_DIR/home/$FILE >> ~/$FILE
+    FILE_PATH=$SCRIPT_DIR/home/$FILE
+    FILE_HOME_PATH=~/$FILE
+    FILE_VALUE=`cat $FILE_PATH`
+
+    CONTAINS=`cat $FILE_HOME_PATH | grep -q "$FILE_VALUE"; echo $?`
+
+    if [ "$CONTAINS" -eq "0" ]; then
+        echo "Allready loaded $FILE"
+    else
+        cat $FILE_PATH >> $FILE_HOME_PATH
+    fi
 }
 
 linkFile .aliases
 linkFile .bashrc
 linkFile .hushlogin
+linkFile .tmux.conf
 addToFile .zshrc
+
+echo -e '\nDone'
